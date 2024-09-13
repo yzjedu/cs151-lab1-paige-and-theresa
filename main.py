d44cb894e84a8a1bd6e764b3c3f945c39dca4c9c
tadejacimo <tadejacimo@loyola.edu>
2024-09-13 12:48:25 -0400

    to paige

--- main.py
+++ main.py
@@ -9,13 +9,22 @@
 # Data Out: total cost of gas
 # Credits: [in class]
 
+
+#Ask how many miles user is traveling
+
 miles_int = int(input("How many miles are you traveling?"))
+
+#Ask how many miles one gallon fuels for
+
 mpg_int = int(input("How many miles per gallon?"))
+
+#Ask how much one gallon of gas costs
+
 gallon_cost_int = int(input("How much does 1 gallon cost?"))
 
 gallon_num = miles_int / mpg_int
 
 total_cost = gallon_cost_int * gallon_num
 
-print(f'"Total cost:", {total_cost:.2f}')
+print(f'Total cost: {total_cost:.2f}')
 
