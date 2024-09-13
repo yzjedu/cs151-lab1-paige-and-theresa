1e752959da45860981c5bee025d9220cdf270925
pronan <pronan@loyola.edu>
2024-09-13 12:15:33 -0400

    comments

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# Enter code here
-# Read the README for instructions.
-# You must write an algorithm and test cases first!
+# Programmers: Theresa DeJacimo and Paige Ronan
+# Course:  CS151, Dr. Zelalem Jembre Yalew
+# Due Date: 9/25/2024
+# Lab Assignment: 1
+# Problem Statement: [figure out the cost for gas for your trip using
+# how many miles you will travel, the miles per gallon (MPG)
+# of your car, and the cost of gas.]
+# Data In: miles being traveled, miles per gallon, price per gallon
+# Data Out: total cost of gas
+# Credits: [in class]
